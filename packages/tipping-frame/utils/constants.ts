55e0b3e00161826251b38071d8ff2cc154d70f04
Harsh Bajpai <bajpaiharsh244@gmail.com>
2024-02-22 11:28:22 +0530

    fix: fix json parsing

--- packages/tipping-frame/utils/constants.ts
+++ packages/tipping-frame/utils/constants.ts
@@ -1,7 +1,38 @@
-import { validateAndReturnEnv } from "./utils";
-
 export const TIP_CLASS_HASH = "0x155605fd9474736cec781524ed144b3c354aa29384d7ece595f1a55c2f4bceb"
 export const TIP_ADDRESS = "0x1b4a2420aa1fecc9025d8ab13ca32d337339c45b7d7fbb3e2497e46a5c49868"
 export const ETH_ADDRESS = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
 
-export const ENV = validateAndReturnEnv();
+if (process.env.NEYNAR_API_KEY === undefined) {
+    throw new Error("NEYNAR_API_KEY is not set in the environment");
+}
+export const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY;
+
+if (process.env.RPC_URL === undefined) {
+    throw new Error("RPC_URL is not set in the environment");
+}
+export const RPC_URL = process.env.RPC_URL;
+
+if (process.env.PRIVATE_KEY === undefined) {
+    throw new Error("PRIVATE_KEY is not set in the environment");
+}
+export const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+if (process.env.ACCOUNT_ADDRESS === undefined) {
+    throw new Error("ACCOUNT_ADDRESS is not set in the environment");
+}
+export const ACCOUNT_ADDRESS = process.env.ACCOUNT_ADDRESS;
+
+if (process.env.PRIVATE_KEY_SENDER === undefined) {
+    throw new Error("PRIVATE_KEY_SENDER is not set in the environment");
+}
+export const PRIVATE_KEY_SENDER = process.env.PRIVATE_KEY_SENDER;
+
+if (process.env.ACCOUNT_ADDRESS_SENDER === undefined) {
+    throw new Error("ACCOUNT_ADDRESS_SENDER is not set in the environment");
+}
+export const ACCOUNT_ADDRESS_SENDER = process.env.ACCOUNT_ADDRESS_SENDER;
+
+if (process.env.CONTRACT_BUILD_PATH === undefined) {
+    throw new Error("CONTRACT_BUILD_PATH is not set in the environment");
+}
+export const CONTRACT_BUILD_PATH = process.env.CONTRACT_BUILD_PATH;
--- packages/tipping-frame/utils/utils.ts
+++ packages/tipping-frame/utils/utils.ts
@@ -1,6 +1,6 @@
 import { Account, Contract, RpcProvider } from "starknet";
 import fs from "fs";
-import { ENV, TIP_ADDRESS } from './constants'
+import { CONTRACT_BUILD_PATH, TIP_ADDRESS } from './constants'
 import path from "path";
 
 export const getOwnerAccount = (provider: RpcProvider) => {
@@ -26,61 +26,11 @@ export const getContract = (account: Account) => {
 }
 
 export const getSierra = () => {
-    const compiledTestSierra = JSON.parse(fs.readFileSync(path.join(ENV.CONTRACT_BUILD_PATH, "tipping_Tip.contract_class.json")).toString("ascii"));
+    const compiledTestSierra = JSON.parse(fs.readFileSync(path.join(CONTRACT_BUILD_PATH, "tipping_Tip.contract_class.json")).toString("ascii"));
     return compiledTestSierra
 }
 
 export const getCasm = () => {
-    const casm = JSON.parse(fs.readFileSync(path.join(ENV.CONTRACT_BUILD_PATH, "./build/tipping_Tip.compiled_contract_class.json")).toString("ascii"));
+    const casm = JSON.parse(fs.readFileSync(path.join(CONTRACT_BUILD_PATH, "./tipping_Tip.compiled_contract_class.json")).toString("ascii"));
     return casm
-}
-
-export const validateAndReturnEnv = () => {
-
-    const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY;
-    if (NEYNAR_API_KEY === undefined) {
-        throw new Error("NEYNAR_API_KEY is not set in the environment");
-    }
-
-    const RPC_URL = process.env.RPC_URL;
-    if (RPC_URL === undefined) {
-        throw new Error("RPC_URL is not set in the environment");
-    }
-
-    const PRIVATE_KEY = process.env.PRIVATE_KEY;
-    if (PRIVATE_KEY === undefined) {
-        throw new Error("PRIVATE_KEY is not set in the environment");
-
-    }
-
-    const ACCOUNT_ADDRESS = process.env.ACCOUNT_ADDRESS;
-    if (ACCOUNT_ADDRESS === undefined) {
-        throw new Error("ACCOUNT_ADDRESS is not set in the environment");
-    }
-
-    const PRIVATE_KEY_SENDER = process.env.PRIVATE_KEY_SENDER;
-    if (PRIVATE_KEY_SENDER === undefined) {
-        throw new Error("PRIVATE_KEY_SENDER is not set in the environment");
-
-    }
-
-    const ACCOUNT_ADDRESS_SENDER = process.env.ACCOUNT_ADDRESS_SENDER;
-    if (ACCOUNT_ADDRESS_SENDER === undefined) {
-        throw new Error("ACCOUNT_ADDRESS_SENDER is not set in the environment");
-    }
-
-    const CONTRACT_BUILD_PATH = process.env.CONTRACT_BUILD_PATH;
-    if (CONTRACT_BUILD_PATH === undefined) {
-        throw new Error("CONTRACT_BUILD_PATH is not set in the environment");
-    }
-
-    return {
-        NEYNAR_API_KEY,
-        RPC_URL,
-        PRIVATE_KEY,
-        ACCOUNT_ADDRESS,
-        PRIVATE_KEY_SENDER,
-        ACCOUNT_ADDRESS_SENDER,
-        CONTRACT_BUILD_PATH
-    }
 }
